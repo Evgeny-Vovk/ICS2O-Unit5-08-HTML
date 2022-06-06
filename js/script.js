1b59347cb3017042669afa32144acf3242eaa14d
Evgeny Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-06-06 05:15:45 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -14,14 +14,27 @@ if (navigator.serviceWorker) {
 function calculate() {
 
   //input
-  const number = parseInt(document.getElementById('number').value)
-  var answer = 0
+  const number1 = parseInt(document.getElementById('number1').value)
+  const number2 = parseInt(document.getElementById('number2').value)
+  var repeat = number1
+  var fullNumber = 0
 
   // process
-  for (repeat = 0; repeat <= number; repeat++) {
-    answer += repeat
+  if ((number1 > 0) && (number2 > 0)){
+    while (repeat >= number2) {
+      repeat = repeat - number2
+      ++fullNumber
+    }
+  }
+  else if ((number1 < 0) && (number2 < 0)){
+    number2 = Math.abs(number2)
+    repeat = Math.abs(repeat)
+    while (repeat >= number2) {
+      repeat = repeat - number2
+      ++fullNumber
+    }
   }
   
   //output
-  document.getElementById("answer").innerHTML = "The answer is: " + answer
+  document.getElementById("answer").innerHTML = "The answer is: " + repeat
 }
